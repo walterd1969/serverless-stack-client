f451a594b79817f56e374c037661cfab4e971ccc
Walter Dorawa <oracledriver@yahoo.com>
2020-05-22 13:50:08 -0400

    Setting up the React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "walterdorawa-notes-app-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://l7wa0im2ki.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_0gZxWUrJG",
+    APP_CLIENT_ID: "534ccp4gufer5s6ke2qpc45pbu",
+    IDENTITY_POOL_ID: "us-east-1:92e52a80-13a6-4196-9119-c2ab6e257a7d"
+  }
+};
